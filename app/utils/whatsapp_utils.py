11c7aa5c970a17a0a832f2cc2228fccd30db83eb
juaniponce01 <62446939+juaniponce01@users.noreply.github.com>
2024-03-01 01:11:48 -0300

    added downloading audio functions

--- app/utils/whatsapp_utils.py
+++ app/utils/whatsapp_utils.py
@@ -5,6 +5,7 @@ import requests
 
 # from app.services.openai_service import generate_response
 import re
+import os
 
 
 def log_http_response(response):
@@ -23,13 +24,66 @@ def get_text_message_input(recipient, text):
             "text": {"preview_url": False, "body": text},
         }
     )
+    
 
+def get_audio_file_url(media_id):
+    url = f"https://graph.facebook.com/v19.0/{media_id}/"
+    headers = {
+        "Authorization": f"Bearer {current_app.config['ACCESS_TOKEN']}"
+    }
 
-def generate_response(response):
-    # Return text in uppercase
-    return response.upper()
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+        print(data)
+        audio_url = data["url"]
+        print(audio_url)
+        return audio_url
+    except requests.RequestException as e:
+        logging.error(f"Failed to get audio file URL: {e}")
+        return None
+    
 
+def download_audio_file(media_id, file_path):
+    url = get_audio_file_url(media_id)
+    headers = {
+        "Authorization": f"Bearer {current_app.config['ACCESS_TOKEN']}"
+    }
 
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        with open(file_path, "wb") as file:
+            file.write(response.content)
+        return True
+    except requests.RequestException as e:
+        logging.error(f"Failed to download audio file: {e}")
+        return False
+    
+
+def upload_audio_file(media_id, file_path):
+    url = f"https://graph.facebook.com/v19.0/{media_id}/media"
+    headers = {
+        "Authorization": f"Bearer {current_app.config['ACCESS_TOKEN']}"
+    }
+    files = {
+        "file": open(file_path, "rb")
+    }
+    data = {
+        "type": "audio/ogg",
+        "messaging_product": "whatsapp"
+    }
+
+    try:
+        response = requests.post(url, headers=headers, files=files, data=data)
+        response.raise_for_status()
+        return response
+    except requests.RequestException as e:
+        logging.error(f"Failed to upload audio file: {e}")
+        return False
+    
+        
 def send_message(data):
     headers = {
         "Content-type": "application/json",
@@ -80,17 +134,18 @@ def process_whatsapp_message(body):
     name = body["entry"][0]["changes"][0]["value"]["contacts"][0]["profile"]["name"]
 
     message = body["entry"][0]["changes"][0]["value"]["messages"][0]
-    message_body = message["text"]["body"]
+    audio_id = message["audio"]["id"]
+    
+    # Upload audio file
+    file_path = os.path.join(f"audios/{audio_id}.ogg")
+    
+    download_audio_file(audio_id, file_path)
 
-    # TODO: implement custom function here
-    response = generate_response(message_body)
-
-    # OpenAI Integration
-    # response = generate_response(message_body, wa_id, name)
-    # response = process_text_for_whatsapp(response)
-
-    data = get_text_message_input(current_app.config["RECIPIENT_WAID"], response)
-    send_message(data)
+    """ TODO: 
+        - implementar integracion con whisper usando un 'servicio' file
+        response = generate_response(message_body)
+        - crear logica para cuando nos envian un texto en vez de un audio
+    """
 
 
 def is_valid_whatsapp_message(body):
